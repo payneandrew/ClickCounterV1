4a3c2f5556b5d7348bced379f1c83649615ed570
Andrew Payne <andrew.payne@willowtreeapps.com>
2021-04-07 12:01:53 -0600

    Initial Commit

---
+++ ClickCounter/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  ClickCounter
+//
+//  Created by Andrew Payne on 4/7/21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
